e3637d02879bb12d696369578ab5edf7c924b42e
Dmitry <dima.fenix@list.ru>
2022-02-19 15:39:10 +0300

    Added mosaika with random color.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,10 +36,12 @@ namespace WinFormsCG
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.Mosaic = new System.Windows.Forms.GroupBox();
+            this.right_panel = new System.Windows.Forms.GroupBox();
+            this.start_mosaika = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
+            this.right_panel.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -84,18 +86,20 @@ namespace WinFormsCG
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.pictureBox1.Location = new System.Drawing.Point(0, 24);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(673, 418);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+          
+           
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.BurlyWood;
-            this.panel1.Controls.Add(this.Mosaic);
+            this.panel1.Controls.Add(this.right_panel);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel1.Location = new System.Drawing.Point(679, 24);
             this.panel1.Name = "panel1";
@@ -103,15 +107,26 @@ namespace WinFormsCG
             this.panel1.TabIndex = 2;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // Mosaic
+            // right_panel
             // 
-            this.Mosaic.Location = new System.Drawing.Point(3, 4);
-            this.Mosaic.Name = "Mosaic";
-            this.Mosaic.Size = new System.Drawing.Size(153, 411);
-            this.Mosaic.TabIndex = 0;
-            this.Mosaic.TabStop = false;
-            this.Mosaic.Text = "Mosaic";
-            this.Mosaic.Enter += new System.EventHandler(this.groupBox1_Enter);
+            this.right_panel.Controls.Add(this.start_mosaika);
+            this.right_panel.Location = new System.Drawing.Point(3, 4);
+            this.right_panel.Name = "right_panel";
+            this.right_panel.Size = new System.Drawing.Size(153, 411);
+            this.right_panel.TabIndex = 0;
+            this.right_panel.TabStop = false;
+            this.right_panel.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // start_mosaika
+            // 
+            this.start_mosaika.Location = new System.Drawing.Point(6, 372);
+            this.start_mosaika.Name = "start_mosaika";
+            this.start_mosaika.Size = new System.Drawing.Size(138, 33);
+            this.start_mosaika.TabIndex = 0;
+            this.start_mosaika.Text = "start";
+            this.start_mosaika.UseVisualStyleBackColor = true;
+            this.start_mosaika.Visible = false;
+            this.start_mosaika.Click += new System.EventHandler(this.start_mosaika_Click);
             // 
             // Form1
             // 
@@ -119,8 +134,8 @@ namespace WinFormsCG
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(838, 442);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -130,6 +145,7 @@ namespace WinFormsCG
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.right_panel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,7 +160,8 @@ namespace WinFormsCG
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.GroupBox Mosaic;
+        private System.Windows.Forms.GroupBox right_panel;
+        private System.Windows.Forms.Button start_mosaika;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,28 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace WinFormsCG
 {
     public partial class Form1 : Form
     {
+        private Graphics graphics;
+        private Pen pen;
+        private Random random= new Random();
+        private Brush brush;
         public Form1()
         {
             InitializeComponent();
+            graphics = pictureBox1.CreateGraphics();
+            pen = new Pen(Color.Black,10);
+            random = new Random();
         }
 
         private void task1ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            right_panel.Text = "Mosaika";
+            start_mosaika.Visible = true;
 
         }
 
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
+      
 
         private void Form1_Load(object sender, EventArgs e)
         {
+           
 
         }
 
@@ -42,5 +50,31 @@ namespace WinFormsCG
         {
 
         }
+       
+
+        private void start_mosaika_Click(object sender, EventArgs e)
+        {
+            int widthBox = 20;
+
+            for (int b = 0; b < pictureBox1.Height; b++)
+            {
+                for (int i = 0; i < pictureBox1.Width; i++)
+                {
+                    brush = new SolidBrush(Color.FromArgb(255, (byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256)));
+
+                    Rectangle rectangle = new Rectangle(i* widthBox, b* widthBox, widthBox, widthBox);
+                    graphics.FillRectangle(brush, rectangle);   
+                }
+            }
+            
+        } 
+
+          
+          
+
+
+        
+
+
     }
 }
